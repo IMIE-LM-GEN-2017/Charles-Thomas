3d595a03f74bb70606ab8e812550e807c839863a
maclune <maclune@live.fr>
2017-08-14 18:09:51 +0200

    Mise a jour
    Réalisation page  recherche

--- serveur/recherche.php
+++ serveur/recherche.php
@@ -7,11 +7,120 @@
   $titrePage= "Recherche";
   include('design/header.php');
 ?>
+  <div class="container-fluid">
+    <div class="row">
+      <!-- Profil -->
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+         <img src="img/profil.jpg" alt="...">
+         <div class="caption">
+           <h3>Pseudo</h3>
+           <h5><strong>Pays:</strong> label</h5>
+           <h5><strong>Ville:</strong> label</h5>
+           <h5><strong>Age:</strong> label</h5>
+           <h5><strong>Sexe:</strong> label</h5>
+           <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+         </div>
+       </div>
+      </div>
 
-<div class="container-fluid">
-  <div class="row" id="page">
-    <div class="col-md-1" href="index.html"> Logo </div>
-    <div class="col-md-10"></div>
-    <div class="col-md-1 text-center"> Menu </div>
-    </body>
-</html>
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+         <img src="img/profil.jpg" alt="...">
+         <div class="caption">
+           <h3>Pseudo</h3>
+           <h5><strong>Pays:</strong> label</h5>
+           <h5><strong>Ville:</strong> label</h5>
+           <h5><strong>Age:</strong> label</h5>
+           <h5><strong>Sexe:</strong> label</h5>
+           <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+         </div>
+       </div>
+      </div>
+
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+         <img src="img/profil.jpg" alt="...">
+         <div class="caption">
+           <h3>Pseudo</h3>
+           <h5><strong>Pays:</strong> label</h5>
+           <h5><strong>Ville:</strong> label</h5>
+           <h5><strong>Age:</strong> label</h5>
+           <h5><strong>Sexe:</strong> label</h5>
+           <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+         </div>
+       </div>
+      </div>
+
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+         <img src="img/profil.jpg" alt="...">
+         <div class="caption">
+           <h3>Pseudo</h3>
+           <h5><strong>Pays:</strong> label</h5>
+           <h5><strong>Ville:</strong> label</h5>
+           <h5><strong>Age:</strong> label</h5>
+           <h5><strong>Sexe:</strong> label</h5>
+           <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+         </div>
+       </div>
+      </div>
+
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+         <img src="img/profil.jpg" alt="...">
+         <div class="caption">
+           <h3>Pseudo</h3>
+           <h5><strong>Pays:</strong> label</h5>
+           <h5><strong>Ville:</strong> label</h5>
+           <h5><strong>Age:</strong> label</h5>
+           <h5><strong>Sexe:</strong> label</h5>
+           <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+         </div>
+       </div>
+      </div>
+
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+         <img src="img/profil.jpg" alt="...">
+         <div class="caption">
+           <h3>Pseudo</h3>
+           <h5><strong>Pays:</strong> label</h5>
+           <h5><strong>Ville:</strong> label</h5>
+           <h5><strong>Age:</strong> label</h5>
+           <h5><strong>Sexe:</strong> label</h5>
+           <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+         </div>
+       </div>
+      </div>
+
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+         <img src="img/profil.jpg" alt="...">
+         <div class="caption">
+           <h3>Pseudo</h3>
+           <h5><strong>Pays:</strong> label</h5>
+           <h5><strong>Ville:</strong> label</h5>
+           <h5><strong>Age:</strong> label</h5>
+           <h5><strong>Sexe:</strong> label</h5>
+           <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+         </div>
+       </div>
+      </div>
+
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+         <img src="img/profil.jpg" alt="...">
+         <div class="caption">
+           <h3>Pseudo</h3>
+           <h5><strong>Pays:</strong> label</h5>
+           <h5><strong>Ville:</strong> label</h5>
+           <h5><strong>Age:</strong> label</h5>
+           <h5><strong>Sexe:</strong> label</h5>
+           <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+         </div>
+       </div>
+      </div>
+
+    </div>
+  </div>
