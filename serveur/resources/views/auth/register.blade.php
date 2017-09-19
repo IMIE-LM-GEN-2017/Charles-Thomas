bc86d61835c2fbadc971a8af59d398439d51e26b
charles <charles.renaultclogenson@gmail.com>
2017-09-19 10:25:32 +0200

    commit

--- serveur/resources/views/auth/register.blade.php
+++ serveur/resources/views/auth/register.blade.php
@@ -30,7 +30,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Inscription tesssst</div>
+                <div class="panel-heading">Inscription</div>
 
                 <div class="panel-body">
                     <form method="POST" action="{{ route('register') }}">
